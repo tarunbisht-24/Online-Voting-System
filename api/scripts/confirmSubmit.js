da55bbbbf63bdcef8e9e802d4ebb29a26f2fc8a4
Tarun Bisht <bishttarun2002@gmail.com>
2022-04-06 14:17:08 +0530

    Create confirmSubmit.js

---
+++ api/scripts/confirmSubmit.js
@@ -0,0 +1,7 @@
+document.getElementById("submit").style.display="none"
+function confirm(){
+    document.getElementById("submit").style.display="inline"
+}
+function cancelConfirm(){
+    document.getElementById("submit").style.display="none"
+}
